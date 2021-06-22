a8ded692f2ffacd515bb7f9362361fedf60d6b17
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-22 13:25:26 +0900

    Settings

---
+++ src/main/java/jpa/study/demojpainvestment/DemoJpaInvestmentApplication.java
@@ -0,0 +1,13 @@
+package jpa.study.demojpainvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJpaInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpa/study/demojpainvestment/DemoJpaInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.study.demojpainvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
