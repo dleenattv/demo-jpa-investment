882662cab2b29e32a5dfb0f2f9662de422b113dc
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-22 16:25:42 +0900

    Refactor : delete empty line in Repositories

--- src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
@@ -4,5 +4,4 @@ import jpa.study.demojpainvestment.domain.investment.entity.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
-    
 }
--- src/main/java/jpa/study/demojpainvestment/domain/investor/repository/InvestorRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/investor/repository/InvestorRepository.java
@@ -4,5 +4,4 @@ import jpa.study.demojpainvestment.domain.investor.entity.Investor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvestorRepository extends JpaRepository<Investor, Long> {
-    
 }
