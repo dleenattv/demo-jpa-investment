649f1bb7f26180b775f76eff2f3ba6b4e9c843ef
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-22 14:08:29 +0900

    Add InvestorRepository

---
+++ src/main/java/jpa/study/demojpainvestment/domain/investor/repository/InvestorRepository.java
@@ -0,0 +1,8 @@
+package jpa.study.demojpainvestment.domain.investor.repository;
+
+import jpa.study.demojpainvestment.domain.investor.entity.Investor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvestorRepository extends JpaRepository<Investor, Long> {
+    
+}
