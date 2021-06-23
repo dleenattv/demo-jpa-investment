94e0d7110b27d310db5356803dd025f340833ef9
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-23 16:56:54 +0900

    Add findProductsByIsOnSale in ProductRepository

--- src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
@@ -3,5 +3,10 @@ package jpa.study.demojpainvestment.domain.product.repository;
 import jpa.study.demojpainvestment.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findProductsByIsOnSale(Boolean isOnSale);
+
 }
