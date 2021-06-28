58057f7e85d817cd178f9ae95ebe518daa15327e
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 23:20:31 +0900

    Refactor findProductByProductIdAndIsOnSale in ProductRepository

--- src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findProductsByIsOnSale(Boolean isOnSale);
 
-//    Optional findProductByProductId(Long productId);
-
-    Product findProductByProductId(Long productId);
+    Optional<Product> findProductByProductIdAndIsOnSale(Long productId, Boolean isOnSale);
 }
