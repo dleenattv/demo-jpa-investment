f03452f1b17a7b6a05764b74d435223bf2c75c67
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 21:19:56 +0900

    Add findMyProducts in InvestmentController

--- src/main/java/jpa/study/demojpainvestment/api/controller/InvestmentController.java
+++ src/main/java/jpa/study/demojpainvestment/api/controller/InvestmentController.java
@@ -3,8 +3,13 @@ package jpa.study.demojpainvestment.api.controller;
 import jpa.study.demojpainvestment.api.dto.InvestmentCreateDto;
 import jpa.study.demojpainvestment.domain.investment.entity.Investment;
 import jpa.study.demojpainvestment.domain.investment.service.InvestmentService;
+import jpa.study.demojpainvestment.domain.product.entity.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class InvestmentController {
@@ -18,5 +23,10 @@ public class InvestmentController {
     @PostMapping("/investments")
     public Investment investProduct(InvestmentCreateDto investmentCreateDto) throws Throwable {
         return investmentService.investProduct(investmentCreateDto);
+    }
+
+    @GetMapping("/products/{investorId}")
+    public List<Product> findMyProducts(@PathVariable String investorId) throws Throwable {
+        return investmentService.findMyProductsBy(investorId);
     }
 }
