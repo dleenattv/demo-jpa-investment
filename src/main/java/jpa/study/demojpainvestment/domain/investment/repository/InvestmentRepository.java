b5cf2a684d8a5bf97d9aea04071e209066e6717f
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 23:38:14 +0900

    Delete findInvestmentsByInvestor in InvestmentRepository

--- src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
@@ -1,15 +1,10 @@
 package jpa.study.demojpainvestment.domain.investment.repository;
 
 import jpa.study.demojpainvestment.domain.investment.entity.Investment;
-import jpa.study.demojpainvestment.domain.investor.entity.Investor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
-
-    List<Investment> findInvestmentsByInvestor(Investor investor);
 
 }
