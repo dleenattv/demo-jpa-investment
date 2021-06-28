bbd4bef158e20bcd182c334310163ef9805e41f9
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 16:45:58 +0900

    Add findInvestmentsByInvestor in InvestmentRepository

--- src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/investment/repository/InvestmentRepository.java
@@ -1,7 +1,14 @@
 package jpa.study.demojpainvestment.domain.investment.repository;
 
 import jpa.study.demojpainvestment.domain.investment.entity.Investment;
+import jpa.study.demojpainvestment.domain.investor.entity.Investor;
+import jpa.study.demojpainvestment.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+
+    List<Investment> findInvestmentsByInvestor(Investor investor);
+
 }
