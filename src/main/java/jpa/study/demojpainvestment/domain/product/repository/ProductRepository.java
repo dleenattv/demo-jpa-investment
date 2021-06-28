0a16a1507ded2df2617069b7507600e49afbb1ca
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 15:50:29 +0900

    Add findProductByProductId in ProductRepository

--- src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/product/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import jpa.study.demojpainvestment.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findProductsByIsOnSale(Boolean isOnSale);
 
+    Optional findProductByProductId(Long productId);
 }
