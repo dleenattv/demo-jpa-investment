c7ae689cc314d9118b16766f24ac066cf9f9bfdf
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 15:53:55 +0900

    Add findInvestorByInvestorId in InvestorRepository

--- src/main/java/jpa/study/demojpainvestment/domain/investor/repository/InvestorRepository.java
+++ src/main/java/jpa/study/demojpainvestment/domain/investor/repository/InvestorRepository.java
@@ -3,5 +3,10 @@ package jpa.study.demojpainvestment.domain.investor.repository;
 import jpa.study.demojpainvestment.domain.investor.entity.Investor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InvestorRepository extends JpaRepository<Investor, Long> {
+
+    Optional findInvestorByInvestorId(String investorId);
+
 }
