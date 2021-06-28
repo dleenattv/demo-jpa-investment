46b9695ae47441c3dea542fe94b66c6b5cd68ae8
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-28 16:49:37 +0900

    Add findAllProducts in ProductController

--- src/main/java/jpa/study/demojpainvestment/api/controller/ProductController.java
+++ src/main/java/jpa/study/demojpainvestment/api/controller/ProductController.java
@@ -3,8 +3,11 @@ package jpa.study.demojpainvestment.api.controller;
 import jpa.study.demojpainvestment.api.dto.ProductCreateDto;
 import jpa.study.demojpainvestment.domain.product.entity.Product;
 import jpa.study.demojpainvestment.domain.product.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -18,5 +21,10 @@ public class ProductController {
     @PostMapping("/products")
     public Product createProduct(ProductCreateDto productCreateDto) {
         return productService.createProduct(productCreateDto);
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts() {
+        return productService.findAllProducts();
     }
 }
